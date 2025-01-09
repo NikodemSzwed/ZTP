a5c309af70874bba83a1d348ed6c993ad673870d
Nikodem Szwed <48102091+NikodemSzwed@users.noreply.github.com>
2025-01-09 17:36:19 +0100

    win compile powershell works

--- compile_win.ps1
+++ compile_win.ps1
@@ -1,29 +1,20 @@
-# Set the root directory of your project
 $projectDir = "."
+$outDir = "out"
 
-# Set the output directory for compiled .class files (e.g., "out")
-$outDir = "$projectDir\out"
-
-# Create the output directory if it doesn't exist
 if (-Not (Test-Path -Path $outDir)) {
     New-Item -ItemType Directory -Path $outDir
 }
 
-# Clean the output directory (optional)
 Remove-Item -Path "$outDir\*" -Force
 
-# Find all .java files in the project and its subdirectories
 $javaFiles = Get-ChildItem -Recurse -Path $projectDir -Filter "*.java"
+$javaFilePaths = $javaFiles.FullName | ForEach-Object { "`"$_`"" }
+javac -d $outDir $javaFilePaths
 
-# Compile the .java files using javac
-$javacArgs = $javaFiles.FullName -join " "
-javac -d $outDir $javacArgs
 
-# Check if the compilation was successful
 if ($LASTEXITCODE -eq 0) {
     Write-Host "Compilation successful!"
 
-    # Run the program (assuming Main is the entry point class)
     java -cp $outDir com.ztp.ztpproject.Main
 } else {
     Write-Host "Compilation failed!"
