d58c6480c0863b77e871f163f5cea9b34cec07c0
Marbur12 <M.burczynski12@gmail.com>
2025-01-18 13:09:21 +0100

    new tests in main (old main states moved to main_old.java)

--- src/main/java/com/ztp/ztpproject/Main_old.java
+++ src/main/java/com/ztp/ztpproject/Main_old.java
@@ -4,17 +4,16 @@ import com.ztp.ztpproject.builder.RaportDirector;
 import com.ztp.ztpproject.command.AddTagCommand;
 import com.ztp.ztpproject.command.ChangeNameCommand;
 import com.ztp.ztpproject.command.CommandManager;
-import com.ztp.ztpproject.flyweight.*;
-import com.ztp.ztpproject.memento.NoteCaretaker;
-import com.ztp.ztpproject.memento.NoteMemento;
 import com.ztp.ztpproject.models.*;
-import com.ztp.ztpproject.prototype.Template;
+import com.ztp.ztpproject.flyweight.CategoryFactory;
+import com.ztp.ztpproject.memento.NoteCaretaker;
+
+import java.util.Calendar;
 import java.nio.charset.Charset;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Date;
 
-public class Main {
+public class Main_old {
 
     public static void main(String[] args) {
         String os = System.getProperty("os.name").toLowerCase();
@@ -34,8 +33,43 @@ public class Main {
             System.out.println("Non-Windows platform detected. No changes needed.");
         }
 
+        // try {
+        //     System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, "UTF-8"));
+        // } catch (UnsupportedEncodingException e) {
+        //     throw new InternalError("VM does not support mandatory encoding UTF-8");
+        // }
+        // PrintWriter consoleOut = new PrintWriter(new OutputStreamWriter(System.out, "Cp850"));
+        // consoleOut.println(filename);
         System.out.println("\n\n================== TESTY PROJEKTU ZTP ==================\n");
 
+        // Note notePrototype = new Note("Prototype note", "TestNote",
+        // Arrays.asList("TestWażne"));
+        // Template noteTemplate = new Template(notePrototype);
+        // Note customNote = (Note) noteTemplate.CloneCustomPrototype("Custom note",
+        // "Custom note content");
+        // customNote.showDetails();
+        // Task defaultTask = (Task) taskTemplate.CloneDefaultPrototype();
+        // defaultTask.showDetails();
+        // Note defaultNote = (Note) noteTemplate.CloneDefaultPrototype();
+        // defaultNote.showDetails();
+        // // Test TagFactory
+        // TagFactory tagFactory = new TagFactory();
+        // Tag urgentTag1 = tagFactory.getState("Urgent");
+        // Tag urgentTag2 = tagFactory.getState("Urgent");
+        // System.out.println("Czy Tag 'Urgent' jest współdzielony: " + (urgentTag1 ==
+        // urgentTag2)); // true
+        // System.out.println("Nieistniejący Tag: " +
+        // tagFactory.getStateDontAdd("Optional")); // null
+        // System.out.println("Wszystkie tagi: " + tagFactory.getAllStates());
+        /**
+         * ten kod testuje user,role, task(category (factory i proxy)),*
+         */
+        // raportDirector(txtBuilder), template(ElementPrototype)
+        // do dodania do testów note(template from note, command, memento, tag) -
+        // wcześniejsze wywołania części można znaleźć wyżej
+        // TODO
+        // opisy java doc
+        // memento i command
         Calendar calendar = Calendar.getInstance();
         calendar.set(2025, Calendar.JANUARY, 10);
         Date date = calendar.getTime();
@@ -44,18 +78,18 @@ public class Main {
         calendar.set(2025, Calendar.JANUARY, 16);
         Date date3 = calendar.getTime();
 
+        CategoryFactory categoryFactory = CategoryFactory.getInstance();
+
         User userN = new User("Nikodem", RoleList.ADMIN);
         User userM = new User("Marcin", RoleList.MODERATOR);
         User userD = new User("Damian", RoleList.USER);
 
-        CategoryFactoryProxy categoryFactoryProxy = new CategoryFactoryProxy(userN);
-
         userN.addTask("Zadanie 1", "Opis zadania 1", 1, date, Arrays.asList("Work", "Personal"));
-        System.out.println("Wszystkie Kategorie: " + categoryFactoryProxy.getAllStates());
+        System.out.println("Wszystkie Kategorie: " + categoryFactory.getAllStates());
         userM.addTask("Zadanie 2", "Opis zadania 2", 2, date2, Arrays.asList("Work2", "Personal"));
-        System.out.println("Wszystkie Kategorie: " + categoryFactoryProxy.getAllStates());
+        System.out.println("Wszystkie Kategorie: " + categoryFactory.getAllStates());
         userD.addTask("Zadanie 3", "Opis zadania 3", 3, date3, Arrays.asList("Work3", "Personal"));
-        System.out.println("Wszystkie Kategorie: " + categoryFactoryProxy.getAllStates());
+        System.out.println("Wszystkie Kategorie: " + categoryFactory.getAllStates());
 
         System.out.println("Zadania użytkownika " + userN.getName() + ": " + userN.getTaskList());
         userN.saveAsTemplate(userN.getTask(0));
@@ -69,53 +103,16 @@ public class Main {
         calendar.set(2025, Calendar.JANUARY, 15);
         date2 = calendar.getTime();
         director.generateRaportTxt(userN.getTaskList(), date, date2);
+        // director.generateRaportPdf(userN.getTaskList(), date, date2);
 
+        //memento jest używane jako ogólna historia notatki
+        //natomiast command jest wykorzystywane do edycji notatki - i jego historia ogranicza się do operacji edycji
         userN.addNote("Notatka 1", "Opis notatki", Arrays.asList("Note Work"));
         NoteCaretaker noteCaretaker = userN.getNoteCareTaker(0);
-        Note.ReadOnlyNote readOnlyNote = noteCaretaker.getReadOnlyOriginator();
-        Note note = new Note(readOnlyNote.getName(), readOnlyNote.getContent(), readOnlyNote.getTags());
-
-        // Save multiple states of the note
-        noteCaretaker.addMementoFromOriginator();
-        note.setContent("Zmiana treści notatki");
-        noteCaretaker.addMementoFromOriginator();
-        note.setContent("Kolejna zmiana treści notatki");
-        noteCaretaker.addMementoFromOriginator();
-
-        // Print current state
-        System.out.println("Po zmianach: " + note);
-
-        // Restore to previous states
-        NoteMemento memento1 = noteCaretaker.getNoteMemento(1);
-        note.restoreFromMemento(memento1);
-        System.out.println("Po przywróceniu do stanu 1: " + note);
-
-        NoteMemento memento0 = noteCaretaker.getNoteMemento(0);
-        note.restoreFromMemento(memento0);
-        System.out.println("Po przywróceniu do stanu 0: " + note);
-
-        CommandManager<Note> commandManager = new CommandManager<>(noteCaretaker);
+        CommandManager<Note> commandManager = new CommandManager<Note>(noteCaretaker);
         commandManager.executeCommand(new ChangeNameCommand("Notatka 2"));
         commandManager.executeCommand(new AddTagCommand(userN.getTagFactory(), "Work2"));
+
         System.out.println("Notatki użytkownika " + userN.getName() + ": " + userN.getNotesList());
-
-        System.out.println("\n\n================== TESTY DODATKOWE ==================\n");
-
-        System.out.println("Test wzorca Prototype na przykładzie notatek:");
-        Note prototypeNote = new Note("Prototype", "This is a prototype note", Arrays.asList());
-        Template noteTemplate = new Template(prototypeNote);
-        Note clonedNote = (Note) noteTemplate.CloneCustomPrototype("Cloned Note", "Content of cloned note");
-        System.out.println("Oryginał: " + prototypeNote);
-        System.out.println("Klon: " + clonedNote);
-
-        System.out.println("\nTest wzorca Flyweight z Proxy:");
-        TagFactory tagFactory = new TagFactory();
-        Tag urgentTag1 = tagFactory.getState("Urgent");
-        Tag urgentTag2 = tagFactory.getState("Urgent");
-        System.out.println("Czy Tag 'Urgent' jest współdzielony: " + (urgentTag1 == urgentTag2));
-        System.out.println("Nieistniejący Tag: " + tagFactory.getState("Optional"));
-        System.out.println("Wszystkie tagi: " + tagFactory.getAllStates());
-
-        System.out.println("\nTesty zakończone.");
     }
 }
